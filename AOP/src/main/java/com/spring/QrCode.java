1bd8fe643efa74c258fe1bd629861da21f5ed380
Wang-1001 <15151153716q>
2019-03-11 16:25:44 +0800

    commit ORM first

--- AOP/src/main/java/com/spring/QrCode.java
+++ AOP/src/main/java/com/spring/QrCode.java
@@ -10,7 +10,7 @@ public class QrCode {
     public static void main(String[] args) {
         QrConfig config = new QrConfig(300,300);
         config.setMargin(3);
-        config.setImg("E:/A1.jpg");
+        config.setImg("D:/4.png");
         Color foreColor = new Color(238,110,115);
         Color bgColor = new Color(252,228,236);
         int foreColorRGB = foreColor.getRGB();
@@ -22,6 +22,6 @@ public class QrCode {
         config.setBackColor(bgColorRBG);
 
         //
-        QrCodeUtil.generate("https://www.jianshu.com/writer#/notebooks/35012023/notes/42393607",config,FileUtil.file("d:/A.jpg"));
+        QrCodeUtil.generate("https://www.jianshu.com/writer#/notebooks/35012023/notes/42393607",config,FileUtil.file("d:/w.jpg"));
     }
 }
